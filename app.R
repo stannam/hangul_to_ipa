74588970a5c0712ba5282e7adc78e2fac0dad745
stannam <stan.nam98@gmail.com>
2022-07-25 01:09:36 -0700

    aiming for finishing deploying

--- app.R
+++ app.R
@@ -175,6 +175,6 @@ app$callback(
 )
 
 # 4. Run app, change for deploy online
-# app$run_server(host = '0.0.0.0', port = Sys.getenv('PORT', 8050))
+app$run_server(host = '0.0.0.0', port = Sys.getenv('PORT', 8050))
 
-app$run_server(debug = T)  ## local debugging
+##app$run_server(debug = T)  ## local debugging
