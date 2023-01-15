c59bf26200e96fca6bd36dd4f21e7c9856fefd52
stannam <43150234+stannam@users.noreply.github.com>
2023-01-14 23:37:11 -0800

    Update app.R

--- app.R
+++ app.R
@@ -310,6 +310,6 @@ app$callback(
 )
 
 # 4. Run app, change for deploy online
-#app$run_server(host = '0.0.0.0', port = Sys.getenv('PORT', 8050))
+app$run_server(host = '0.0.0.0', port = Sys.getenv('PORT', 8050))
 
 app$run_server(debug = T)  ## local debugging
